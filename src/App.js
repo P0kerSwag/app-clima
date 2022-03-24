6f4c41c1df7775fd8f2b16402b15e70e47fc70b6
C:/Program Files/Git/thiag <thiagochiroquerodriguez@gmail.com>
2022-03-24 15:20:31 -0300

    subindo a base do projeto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+
+const api ={
+  key: "23a3cc0902fe7b8c8192a2f8fbc5be9c",
+  base: "https://api.openweathermap.org/data/2.5/",
+
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Digite a cidade..."
+          />
+        </div>
+      </main>
+
     </div>
   );
 }
